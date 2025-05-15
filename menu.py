3da7467f4e092e06353256efa9ca174609110573
UnormalDude <103203921+UnormalDude@users.noreply.github.com>
2025-05-15 11:23:47 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -23,7 +23,7 @@ def calc_wage():
     print("Or you can input how many hours you can work per week and it will tell you how much you would need to earn per hour.")
 
     current_funds = input("\n\nFirst please type how much money you can currently put towards tuition: ")
-    percentage = input("\nplease type the percentage of the tuition you are looking to pay: \n\t>")
+    percentage = input("\nplease type the percentage of the tuition you are looking to pay: \n\t>  ")
     user_input = input("Would you rather input:\n1 - An Hourly Rate\n2 - Hours you can work a week")
 
     #if user_input == "1":
@@ -34,14 +34,14 @@ def change_coa():
     print("Here you will be asked to provide the costs of different categories. Tuition, Room and board, Food, and Fees.")
     print("\n" + text_edit("bold") + text_edit("red") + "If you do not know a cost, please just enter a zero\n\nOnly numbers, no commas are needed\n" + text_edit("reset"))
 
-    tuition = int(input("Tuition: $"))
-    room = int(input("\nRoom and Board: $"))
-    food = int(input("\nFood: $"))
+    tuition = int(input("Tuition: $ "))
+    room = int(input("\nRoom and Board: $ "))
+    food = int(input("\nFood: $ "))
     
     print(text_edit("red") + text_edit("bold") + "Please input only numbers for fees. Each fee can be put in its own category. When done type \'done\'\n\n" + text_edit("reset"))
     fees = 0
     while True:
-        fee = input("\nFee: $")
+        fee = input("\nFee: $ ")
         if fee == "done":
             break
         else:
